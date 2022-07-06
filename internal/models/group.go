d23c670112060deb870c1f90f9ec9b6aa9eb1c65
utqyp <marius@wedner.de>
2022-07-06 20:53:10 +0200

    Models: Update group.

---
+++ internal/models/group.go
@@ -0,0 +1,28 @@
+package models
+
+type Group struct {
+	ID          string `bson:"_id,omitempty"`
+	GID         string `bson:"GID" json:"GID"`
+	Title       string `bson:"Title" json:Title"`
+	Description string `bson:"Description" json:"Description"`
+	Events      string `bson:"Events" json:"Events"`
+}
+
+func (group Group) CalculateGID() {
+
+}
+
+type GroupRelation struct {
+	Id      string    `bson:"_id,omitempty"`
+	GroupId string    `bson:"GroupId" json:"GroupId"`
+	UserId  string    `bson:"UserId" json:"UserId"`
+	Role    GroupRole `bson:"Role" json:"Role"`
+}
+
+type GroupRole int
+
+const (
+	Owner GroupRole = iota
+	Moderator
+	Member
+)
