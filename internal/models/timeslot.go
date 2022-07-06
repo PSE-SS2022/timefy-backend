b1121bb27949a15a5dc303e47005b47ebb230370
utqyp <marius@wedner.de>
2022-07-06 16:34:59 +0200

    Models: Replace old time implementation with new timeslot implementation.

--- internal/models/timeslot.go
+++ internal/models/timeslot.go
@@ -1,6 +1,16 @@
 package models
 
+import "time"
+
 type TimeSlot struct {
-	BeginTime Time `bson:"BeginTime" json:"BeginTime"`
-	EndTime   Time `bson:"EndTime" json:"EndTime"`
+	StartTime time.Time `bson:"StartTime" json:"StartTime"`
+	EndTime   time.Time `bson:"EndTime" json:"EndTime"`
+}
+
+func (t TimeSlot) StartToString() string {
+	return t.StartTime.GoString()
+}
+
+func (t TimeSlot) EndToString() string {
+	return t.StartTime.GoString()
 }
