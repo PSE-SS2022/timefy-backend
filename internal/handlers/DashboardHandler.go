986a6d398d4af73f851ab20bc076473c68b0a9c5
utqyp <marius@wedner.de>
2022-07-07 21:33:15 +0200

    Handlers: Add Dashboardhandler.

---
+++ internal/handlers/DashboardHandler.go
@@ -0,0 +1,26 @@
+package handlers
+
+//import . "github.com/PSE-SS2022/timefy-backend/internal/models"
+
+type DashboardHandler struct {
+}
+
+func (dashboardHandler DashboardHandler) BanUser(userId string, banDuration int) {
+
+}
+
+func (dashboardHandler DashboardHandler) DeleteEvent(eventId string) {
+
+}
+
+func (dashboardHandler DashboardHandler) CreateModerator(email, password string) {
+
+}
+
+func (dashboardHandler DashboardHandler) DeleteModerator(email string) {
+
+}
+
+func (dashboardHandler DashboardHandler) UnbanUser(userId string) {
+
+}
