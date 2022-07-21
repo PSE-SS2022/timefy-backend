e645845a3c98a39729b51475e140ae3574903a71
Abdullah Yildirim <yildabdu@gmail.com>
2022-07-21 11:46:06 +0200

    deleteUser functionality

--- cmd/handlers/userProfileHandler.go
+++ cmd/handlers/userProfileHandler.go
@@ -1,7 +1,16 @@
 package handlers
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+
+	"github.com/PSE-SS2022/timefy-backend/internal/repos"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func DeleteUser(response http.ResponseWriter, request *http.Request) {
-
+	params := mux.Vars(request)
+	id := params["id"]
+	repos.GetCollection("users").DeleteOne(context.TODO(), bson.D{{Key: "id", Value: id}})
 }
