c5fa8d45966ef78bc1a6615f252ed9095257e7d9
Abdullah Yıldırım <yildabdu@gmail.com>
2022-07-25 01:16:37 +0200

    mock signup handler

--- main.go
+++ main.go
@@ -30,6 +30,8 @@ func main() {
 	router.HandleFunc("/register", handlers.RegisterPageHandler).Methods("GET")
 	router.HandleFunc("/register", handlers.RegisterAdmin).Methods("POST")
 
+	router.HandleFunc("/signup", handlers.SignUpUser).Methods("POST")
+
 	log.Println("All handlers set and ready to listen")
 	server.Start()
 }
