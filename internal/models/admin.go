0057e12d3c59cd1b3a03a60cc72bbaadfb9f1c57
Abdullah Yıldırım <yildabdu@gmail.com>
2022-07-25 02:08:40 +0200

    add get functions

--- internal/models/admin.go
+++ internal/models/admin.go
@@ -1,12 +1,17 @@
 package models
 
 import (
+	"context"
+
+	"github.com/PSE-SS2022/timefy-backend/internal/repos"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 const (
 	ADMIN_ROLE     = "admin"
 	MODERATOR_ROLE = "moderator"
+	ADMIN_REPO     = "admins"
 )
 
 type Admin struct {
@@ -16,4 +21,26 @@ type Admin struct {
 	Email     string             `bson:"Email" json:"Email"`
 	Password  string             `bson:"Password" json:"Password"`
 	Role      string             `bson:"Role" json:"Role"`
+}
+
+func GetAdminById(id primitive.ObjectID) (Admin, bool) {
+	var admin Admin
+	usersCollection := repos.GetCollection(ADMIN_REPO)
+	usersCollection.FindOne(context.TODO(), bson.M{"id": id}).Decode(&admin)
+	if admin.ID.IsZero() {
+		return admin, false
+	}
+	return admin, true
+}
+
+func GetAdminByMail(email string) (Admin, bool) {
+	var admin Admin
+	usersCollection := repos.GetCollection(ADMIN_REPO)
+	err := usersCollection.FindOne(context.TODO(), bson.M{"Email": email}).Decode(&admin)
+	if admin.ID.IsZero() {
+		println("could not decode: " + err.Error())
+		println(email)
+		return admin, false
+	}
+	return admin, true
 }
